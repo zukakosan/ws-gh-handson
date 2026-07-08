fca95f71be7ad03eb581199204d8b11e1fda0ca9
zukakosan <zukakosan@gmail.com>
2026-07-08 15:50:40 +0900

    feat: GET /todos/{todo_id} を実装し各エンドポイントに docstring を追加

--- main.py
+++ main.py
@@ -24,16 +24,19 @@ todos: list[Todo] = [
 
 @app.get("/")
 def read_root():
+    """ウェルカムメッセージを返す。"""
     return {"message": "Welcome to the TODO App!"}
 
 
 @app.get("/todos", response_model=list[Todo])
 def list_todos():
+    """すべての TODO を一覧で返す。"""
     return todos
 
 
 @app.post("/todos", response_model=Todo, status_code=status.HTTP_201_CREATED)
 def create_todo(todo: TodoCreate):
+    """新しい TODO を作成して返す。"""
     new_id = max((t.id for t in todos), default=0) + 1
     new_todo = Todo(id=new_id, title=todo.title, completed=False)
     todos.append(new_todo)
@@ -46,6 +49,7 @@ def create_todo(todo: TodoCreate):
 #
 @app.get("/todos/{todo_id}", response_model=Todo)
 def get_todo(todo_id: int):
+    """指定した id の TODO を返す。見つからない場合は 404 を返す。"""
     for todo in todos:
         if todo.id == todo_id:
             return todo
